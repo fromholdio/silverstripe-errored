b4d748045443e925b2f325fb84b86337933cf34f
Luke Fromhold <luke@fromhold.io>
2025-09-20 23:52:37 +1000

    SS6 upgrade

--- src/Extensions/DbBuildExtension.php
+++ src/Extensions/DbBuildExtension.php
@@ -5,12 +5,13 @@ namespace Fromholdio\Errored\Extensions;
 use Fromholdio\Errored\Errored;
 use SilverStripe\Core\Extension;
 use SilverStripe\Core\Injector\Injector;
+use SilverStripe\PolyExecution\PolyOutput;
 
 class DbBuildExtension extends Extension
 {
-    public function onAfterBuild($quiet, $populate, $testMode)
+    public function onAfterBuild(PolyOutput $output, bool $populate = true, bool $testMode = false)
     {
         $errored = Injector::inst()->create(Errored::class);
-        $errored::writeAllStaticErrors(true, $quiet);
+        $errored::writeAllStaticErrors(true, $output->isQuiet());
     }
 }
